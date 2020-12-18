e51cf0180b58bd467b70934b3df7f1f4d74d6629
Andimas Bagaswara <andimas.bagaswara@gmail.com>
2020-12-18 23:52:48 +0700

    Initial Commit

---
+++ cinebase-ios/cinebase_iosApp.swift
@@ -0,0 +1,20 @@
+//
+//  cinebase_iosApp.swift
+//  cinebase-ios
+//
+//  Created by Andimas Bagaswara on 18/12/20.
+//
+
+import SwiftUI
+
+@main
+struct cinebase_iosApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
