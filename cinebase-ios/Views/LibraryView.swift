81f05c6eed12fa5c4347b44c778b2cd42ea864c5
Andimas Bagaswara <andimas.bagaswara@gmail.com>
2020-12-22 13:02:08 +0700

    Refactor Library View

--- cinebase-ios/Views/LibraryView.swift
+++ cinebase-ios/Views/LibraryView.swift
@@ -12,16 +12,55 @@ struct LibraryView: View {
     @ObservedObject private var libraryVM = LibraryViewModel()
     
     var body: some View {
-        VStack(alignment: .center, spacing: 0, content: {
-            List(content: {
-                ForEach(self.libraryVM.items, id: \.id) { (item) in
-                    URLImage(url: item.backdropPath)
+        ScrollView {
+            LazyVStack {
+                ForEach(self.libraryVM.items, id: \.id) { (itemVM) in
+                    NavigationLink(
+                        destination: MovieDetailScreen(id: itemVM.id.intValue),
+                        label: {
+                            LibraryItem(itemVM: itemVM)
+                        })
                 }
-            })
-        })
+            }
+        }
         .onAppear(perform: {
             self.libraryVM.fetchAllItems()
         })
+    }
+}
+
+struct LibraryItem: View {
+    
+    let itemVM: ItemViewModel
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: nil, content: {
+            URLImage(url: itemVM.backdropPath)
+                .aspectRatio(contentMode: .fill)
+                .cornerRadius(10)
+                .frame(width: Constants.heightRatio * 100, height: Constants.heightRatio * 100, alignment: .center)
+                .clipped()
+                .cornerRadius(10)
+            
+            VStack(alignment: .leading, spacing: nil, content: {
+                Text(itemVM.title)
+                    .font(.customHeading)
+                    .foregroundColor(.black)
+                Text(itemVM.genre)
+                    .font(.customBody)
+                    .foregroundColor(.gray)
+                HStack(alignment: .center, spacing: nil, content: {
+                    Image(systemName: "star.fill")
+                        .font(.system(size: 10))
+                        .foregroundColor(.yellow)
+                    Text("\(itemVM.rating, specifier: "%.1f")/10")
+                        .font(.customBody)
+                        .foregroundColor(.gray)
+                })
+            })
+            Spacer()
+        })
+        .padding()
     }
 }
 
